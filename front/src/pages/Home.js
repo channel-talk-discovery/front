6f6b50740ac58b2e6883754130e83599bdfa1c2b
prectal123 <prectal123@naver.com>
2023-02-17 20:13:20 +0900

    Create Routes

---
+++ front/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div className = "home">
+            <div className="header">
+                This is home
+            </div>
+        </div>
+    );
+};
+
+export default Home;
---
+++ front/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <div className = "main">
+            <div className="header">
+                This is main
+            </div>
+        </div>
+    );
+};
+
+export default Main;
